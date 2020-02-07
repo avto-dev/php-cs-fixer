f18104cb2a9d9143aae1ccae07cf52b3458f3480
Paramtamtam <slashkot@gmail.com>
2020-02-07 12:38:26 +0000

    Apply fixes from StyleCI

--- config.php
+++ config.php
@@ -7,36 +7,36 @@
  */
 
 // Detect project root directory
-$reflector        = new \ReflectionClass('\\Composer\\Autoload\\ClassLoader');
+$reflector = new \ReflectionClass('\\Composer\\Autoload\\ClassLoader');
 $project_root_dir = \dirname(\dirname(\dirname($reflector->getFileName())));
 
-if (! empty($env_cache_file_path = \getenv('PHP_CS_FIX_CACHE_FILE_PATH')) && \is_string($env_cache_file_path)) {
+if (!empty($env_cache_file_path = \getenv('PHP_CS_FIX_CACHE_FILE_PATH')) && \is_string($env_cache_file_path)) {
     $cache_file_path = $env_cache_file_path;
 } else {
     $cache_file_name = '.php_cs.cache';
 
-    $cache_file_path = \is_dir($project_root_dir . '/storage')
-        ? $project_root_dir . '/storage/' . $cache_file_name
-        : $project_root_dir . '/' . $cache_file_name;
+    $cache_file_path = \is_dir($project_root_dir.'/storage')
+        ? $project_root_dir.'/storage/'.$cache_file_name
+        : $project_root_dir.'/'.$cache_file_name;
 }
 
-$rules    = require __DIR__ . '/cs_rules.php';
-$excludes = require __DIR__ . '/cs_excludes.php';
-$config   = new \PhpCsFixer\Config('Avto Develops Code Style Fixer');
+$rules = require __DIR__.'/cs_rules.php';
+$excludes = require __DIR__.'/cs_excludes.php';
+$config = new \PhpCsFixer\Config('Avto Develops Code Style Fixer');
 
 $config
     ->setFinder(PhpCsFixer\Finder::create()
-        ->exclude(\file_exists($user_excludes = $project_root_dir . '/.cs_excludes.php')
+        ->exclude(\file_exists($user_excludes = $project_root_dir.'/.cs_excludes.php')
             ? \array_replace_recursive($excludes, require $user_excludes)
             : $excludes)
         ->in($project_root_dir))
     ->setRiskyAllowed(true)
     ->setUsingCache(true)
-    ->setRules(file_exists($user_rules = $project_root_dir . '/.cs_rules.php')
+    ->setRules(file_exists($user_rules = $project_root_dir.'/.cs_rules.php')
         ? \array_replace_recursive($rules, require $user_rules)
         : $rules);
 
-if (! empty($cache_file_path)) {
+if (!empty($cache_file_path)) {
     $config->setCacheFile($cache_file_path);
 }
 
