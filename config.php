a114f59c712ad947edd3cd4f20cbcd3fbbf75998
Ilia Kislitsin <kislitcin.i@spectrumdata.ru>
2023-04-07 12:06:57 +0500

    updated dependencies and configs

--- config.php
+++ config.php
@@ -34,10 +34,7 @@ $config
     ->setUsingCache(true)
     ->setRules(\file_exists($user_rules = $project_root_dir . '/.cs_rules.php')
         ? \array_replace_recursive($rules, require $user_rules)
-        : $rules);
-
-if (! empty($cache_file_path)) {
-    $config->setCacheFile($cache_file_path);
-}
+        : $rules)
+    ->setCacheFile($cache_file_path);
 
 return $config;
